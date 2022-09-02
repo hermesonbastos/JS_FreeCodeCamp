2bce5ef2da9b930759a110d08f3356d39e856d8f
HBM18 <hermesonbastos@gmail.com>
2022-09-02 16:12:25 -0300

    007

---
+++ JS_básico/007_camel_case.js
@@ -0,0 +1,11 @@
+// Modifique as declarações e atribuições existentes para que seus nomes usem camelCase.
+
+// Declarações de variável
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Atribuições de variável
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
