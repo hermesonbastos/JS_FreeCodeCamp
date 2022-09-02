d43cd3ed3474e0dcec3dc021ab57d19f25f0a9bf
HBM18 <hermesonbastos@gmail.com>
2022-09-02 16:15:48 -0300

    009

---
+++ JS_básico/009_const.js
@@ -0,0 +1,6 @@
+// Altere o código para que todas as variáveis sejam declaradas usando let ou const. Use let quando você quiser que a variável mude e const quando você quiser que a variável permaneça constante. Além disso, renomeie as variáveis declaradas com const para que estejam de acordo com as práticas comuns. Não altere as strings atribuídas às variáveis.
+
+const FCC = "freeCodeCamp"; // Altere esta linha
+let fact = "is cool!"; // Altere esta linha
+fact = "is awesome!";
+console.log(FCC, fact); // Altere esta linha
