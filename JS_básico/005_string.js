d6b7af9388b0f61d7f397a0325564a29955133f8
HBM18 <hermesonbastos@gmail.com>
2022-09-02 16:09:00 -0300

    005

--- JS_básico/005_string.js
+++ JS_básico/005_string.js
@@ -1,2 +1,4 @@
+// Crie duas novas variáveis de string: myFirstName e myLastName e atribua a elas os valores do seu primeiro e último nome, respectivamente.
+
 var myFirstName = "Hermeson";
 var myLastName = "Bastos";
